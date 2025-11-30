0eb1b751b2e6ccc2cca531a448c91483ba19225f
josefinkl <josefin.klangefjall@gmail.com>
2025-11-30 10:01:31 +0100

    some structure

---
+++ game.cpp
@@ -0,0 +1,12 @@
+#include "game.h"
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cctype>
+#include "matrix.h"
+
+void Game::run() {
+    
+
+}
---
+++ game.h
@@ -0,0 +1,16 @@
+#ifndef MINROJ_GAME_H
+#define MINROJ_GAME_H
+
+
+#include <vector>
+#include <string>
+
+
+class Game {
+private:
+   
+
+public:
+    void run();
+};
+#endif //MINROJ_GAME_H
